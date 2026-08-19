66e321669e4d3691722a9ba0418739572e9c5710
Melody Renata <me@lovingmelody.io>
2026-08-19 01:23:52 -0400

    ci: configure nix-ci

---
+++ nix-ci.nix
@@ -0,0 +1,10 @@
+{
+  enable = true;
+  cachix = {
+    name = "nix-citizen";
+    public-key = "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo=";
+  };
+  doNotBuild = [
+    "gameglass"
+  ];
+}
