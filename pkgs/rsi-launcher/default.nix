a6c4f618a2c371125e028ca2a154d4a361b98a87
Melody Renata <me@lovingmelody.io>
2026-08-21 12:23:40 -0400

    rsi-launcher: actually fix prompts by using the correct command

--- pkgs/rsi-launcher/default.nix
+++ pkgs/rsi-launcher/default.nix
@@ -219,7 +219,7 @@ in
                 for trick in "${"\${tricks[@]}"}"; do
                    if [ "${"\${1:-}"}" = "--force-install" ] || (! winetricks list-installed | grep -qw "$trick"); then
                      echo "winetricks: Installing $trick"
-                     y | winetricks -q -f "$trick"
+                     yes | winetricks -q -f "$trick"
                      tricksInstalled=0
                    fi
                 done
